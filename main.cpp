41198ffdbcd21aad5c4d70f0072fb306b2f1ed2e
Zahar <kovalyov.zaxar@bk.ru>
2020-09-21 19:51:20 +0300

    First version

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h> 
+int n; 
+scanf("%d", &n); 
+printf("price: 25$"); 
+printf("ocenka: 9/10"); 
+if n%2 == 0
+	{
+	 printf("v nalichii!"); 
+	} 
+else 
+	{ 
+	printf("net v nalichii!")
+	}
